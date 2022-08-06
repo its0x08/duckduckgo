f5e00d913a4f3e69bbbb57afcdfe468a36de8da0
its0x08 <m.thg@icloud.com>
2022-08-06 19:06:04 +0200

    lint: Fix lint errors

--- ddg.py
+++ ddg.py
@@ -7,18 +7,13 @@ from lib.duckduckgo import DDG
 
 if __name__ == '__main__':
 	try:
-		t = time()
+		start_time = time()
 
-		arguments = ArgumentParser(
-			description='DuckDuckGo URL scraper by 0x08')
-		arguments.add_argument('keyword', metavar='keyword',
-							   type=str, help='search string')
-		arguments.add_argument('-n', dest='max_results',
-							   type=int, help='number of results (default: all)')
-		arguments.add_argument('-t', dest='time',
-							   action=BooleanOptionalAction, default=False, help='execution time')
-		arguments.add_argument('-o', dest='file_output',
-							   type=str, help='save output to file')
+		arguments = ArgumentParser(description='DuckDuckGo URL scraper by 0x08')
+		arguments.add_argument('keyword', metavar='keyword', type=str, help='search string')
+		arguments.add_argument('-n', dest='max_results', type=int, help='number of results (default: all)')
+		arguments.add_argument('-t', dest='time', action=BooleanOptionalAction, default=False, help='execution time')
+		arguments.add_argument('-o', dest='file_output', type=str, help='save output to file')
 
 		args = arguments.parse_args()
 
@@ -28,9 +23,9 @@ if __name__ == '__main__':
 			print(link)
 
 		if args.time:
-			print(f'Execution time: {time()-t:.2f}s')
+			print(f'Execution time: {time()-start_time:.2f}s')
 		elif args.file_output:
-			with open(args.file_output,mode='w', encoding='utf-8') as file_output:
+			with open(args.file_output, mode='w', encoding='utf-8') as file_output:
 				file_output.write('\n'.join(sorted(search)))
 				file_output.write('\n')
 
