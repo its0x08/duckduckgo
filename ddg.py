683aa6cd8517016e41c5197ac112cc5ac8eddc80
its0x08 <mr.thg@icloud.com>
2024-02-09 22:39:06 +0100

    fix: Idenation space to tab

--- ddg.py
+++ ddg.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
 		ARGUMENTS = ArgumentParser(description='DuckDuckGo URL scraper by 0x08')
 		ARGUMENTS.add_argument('keyword', metavar='keyword', type=str, help='search string')
-		ARGUMENTS.add_argument('-n', dest='max_results', type=int, help='number of results (default: all)')
+		ARGUMENTS.add_argument('-n', dest='max_results', type=int, help='number of results(default: all)')
 		ARGUMENTS.add_argument('-t', dest='time', action=BooleanOptionalAction,
 						 default=False, help='execution time')
 		ARGUMENTS.add_argument('-o', dest='file_output', type=str, help='save output to file')
